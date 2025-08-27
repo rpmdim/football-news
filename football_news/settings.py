7cc4f2cc8915d7423b96b5e6f583241a9afab54d
rpmdim <raden.pandji@ui.ac.id>
2025-08-27 16:37:36 +0700

    added my URL deployment PWS

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raden-pandji-footballnews.pbp.cs.ui.ac.id"]
 ...
 
 
